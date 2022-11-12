49b2af4756a5a971bd805f2709879b3d6231a5c6
Arzu <115929474+ArzuGoyushova@users.noreply.github.com>
2022-11-12 18:44:38 +0400

    feat: method string practise

---
+++ NewConsoleApp/NewConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NewConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString("brooklyn"));
+        }
+
+        static string ReverseString (string rev)
+        {
+            string rev2="";
+            
+            for (int i = rev.Length-1; i >=0; i--)
+            {
+                rev2 += rev[i];
+            }
+            return rev2;
+        }
+    }
+}
